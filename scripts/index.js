346c8cbb7c5c19197c10f73ad28c19f72ced247d
Austin Dryden <austinisnorobot@gmail.com>
2019-12-04 13:54:23 -0500

    adds more functions.

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,69 @@
 // Your code goes here!
+
+const numArray = [12,-456,-234,56,3,75,-23,5,47,234,-56,-234,87,5,-978,-132,467,231,-586,-23,586]
+const cities = [
+    { name: 'Los Angeles', temperature: 60.0},
+    { name: 'Atlanta', temperature: 52.0 },
+    { name: 'Detroit', temperature: 48.0 },
+    { name: 'New York', temperature: 80.0 }
+  ];
+const people = [
+    'Dom',
+    'Lyn',
+    'Kirk',
+    'Autumn',
+    'Trista',
+    'Jesslyn',
+    'Kevin',
+    'John',
+    'Eli',
+    'Juan',
+    'Robert',
+    'Keyur',
+    'Jason',
+    'Che',
+    'Ben'
+  ]; 
+
+//positive numbers.
+function positiveNumbers(anArray){
+    return anArray.filter(x => x > 0);
+}
+console.log("all the positive numbers in an array")
+console.log(positiveNumbers(numArray));
+
+//even numbers.
+function evenNumbers(anArray){
+    return anArray.filter(x => (x%2) == 0);
+}
+console.log("all the even numbers in an array")
+console.log(evenNumbers(numArray));
+
+//Square the numbers  in an array
+
+function squareNumbers(numArray){
+    return numArray.map(x => x*x);
+}
+console.log("all the numbers in an array, squared")
+console.log(squareNumbers(numArray));
+
+//return all the cities colder than 70 degrees
+function coolCity(cities){
+    return(cities.filter(city => (city.temperature < 70)).map(x => x.name));
+}
+console.log("cities colder than 70 degrees")
+console.log(coolCity(cities));
+
+//return all the cities in an array of objects
+function allCity(cities){
+    return(cities.map(x => x.name));
+}
+console.log("all the cities");
+console.log(allCity(cities));
+
+// tell everybody what kind of a job they are doing.
+function goodJob(nameArray){
+    return nameArray.map(name => console.log(`Good job, ${name}!!`))
+}
+console.log("GOOD JOB EVERYBODY!!!!!!");
+goodJob(people);    
